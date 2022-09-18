5a54d01cd3d136b3097921c0904eb62a79343c5c
Neil Skaria <neil.skaria@gmail.com>
2022-09-18 09:42:36 -0500

    first alfie model

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,22 @@
 import CanvasExample from '@components/canvas/example';
 import type { CustomPage } from '@components/utils/types/page';
 import DomExample from '@components/dom/example';
+import { Robot } from '@components/models/robot';
+import { Environment } from '@react-three/drei';
 
 const Home: CustomPage = () => {
-	return <DomExample />;
+	// return <DomExample />;
+	return null;
 };
 
 Home.r3f = () => (
 	<>
-		<CanvasExample href="/cube" shape="torus" />
+		{/* <CanvasExample href="/cube" shape="torus" /> */}
+		<Robot />
+		<Environment
+			background={true} // can be true, false or "only" (which only sets the background)
+			preset={'apartment'}
+		/>
 	</>
 );
 
